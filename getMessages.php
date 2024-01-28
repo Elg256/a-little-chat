923db3e04b86bd1805bee906486798bfdd2dc6c2
elg256 <133916011+Elg256@users.noreply.github.com>
2024-01-28 21:07:51 +0100

    Add files via upload

--- getMessages.php
+++ getMessages.php
@@ -3,7 +3,7 @@ global $file;
 $file = "messages.txt";
 
 global $allowedTags;
-$allowedTags = ["br", "h1", "h2", "ul", "ol", "li", "strong", "em", "p", "a", "img"];
+$allowedTags = ["br", "h1", "h2", "ul", "ol", "li", "strong", "em", "p", "a", "img", "video"];
 
 global $maxMessageLength;
 $maxMessageLength = 1000;
@@ -11,6 +11,12 @@ $maxMessageLength = 1000;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $message = $_POST["message"];
+    $pseudo = $_POST["pseudo"];
+    if ($pseudo == "") {
+        $pseudo = "Anonyme";
+    } else {
+        setcookie("pseudo", $pseudo, time() + (60 * 60 * 24 * 30), "/"); // 30 days
+    }
 
     if (!file_exists($file)) {
         touch($file);
@@ -19,7 +25,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (!empty($message) && strlen($message) <= $maxMessageLength) {
         // Add the message to the file
         $messageToSave = nl2br(strip_tags($message, $allowedTags));
-        file_put_contents($file,  date("Y-m-d H:i") . " : " . $messageToSave . PHP_EOL, FILE_APPEND);
+        $pseudoToSave = nl2br(strip_tags($pseudo, $allowedTags));
+        file_put_contents($file,  date("Y-m-d H:i") . " : " . $pseudo . " : " . $messageToSave . PHP_EOL, FILE_APPEND);
     }
 
     // Redirect to the index page
@@ -38,12 +45,18 @@ if (file_exists($file)) {
     foreach ($messages as $message) {
         $messageParts = explode(" : ", $message);
         $date = $messageParts[0];
-
         array_shift($messageParts);
+
+        if (count($messageParts) > 1) {
+            $pseudo = $messageParts[0];
+            array_shift($messageParts);
+        } else {
+            $pseudo = "Anonyme";
+        }
         // Recreate the message
         $messageToDisplay = implode(" : ", $messageParts);
         if ($date > $lastMessageTime) {
-            $newMessages[] = "<div class='sent-message'><span class='date'>$date</span><p>" . $messageToDisplay . "</p></div>";
+            $newMessages[] = "<div class='sent-message'><span class='date'>$date</span> <span class='pseudo'>$pseudo</span><p>" . $messageToDisplay . "</p></div>";
         }
     }
     echo implode("", $newMessages);
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/ico" href="logo_chat.png" />
     <title>Group Chat</title>
     <link rel="stylesheet" href="style.css">
 </head>
@@ -14,15 +15,30 @@
         </div>
         <form action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>' method="post">
             <input type="text" name="message" placeholder="Type your message" autofocus id="messageInput" >
+            <input type="text" name="pseudo" placeholder="Entrez votre pseudo" id="pseudoInput" value="<?php echo isset($_COOKIE['pseudo']) ? htmlspecialchars($_COOKIE['pseudo']) : ''; ?>">
             <button type="submit">Send</button>
         </form>
     </div>
 
-    <div class="container">
-        <form action="index.php" method="post">
-            <button type="submit" id="ReloadButton">Reload</button>
-        </form>
+	<div class="container">
+	
+	
+    <!-- Utilisez une balise label pour styliser le bouton -->
+    <input type="checkbox" id="menu-checkbox">
+    <label for="menu-checkbox" class="menu-label">Menu</label>
+
+    <div class="dropdown">
+        <div class="dropdown-content">
+			<a href="http://stfrancois.alwaysdata.net/index.php#">Tous le monde</a>
+            <a href="http://stfrancoisterminal.alwaysdata.net/index.php#">Terminal</a>
+            <a href="http://stfrancois-premiere.alwaysdata.net/index.php">Première</a>
+            <a href="http://stfrancois-seconde.alwaysdata.net/index.php">Seconde</a>
+            <!-- Ajoutez plus de liens au besoin -->
+        </div>
     </div>
+	
+	
+</div>
 
 	<script src="script.js"></script>
 </body>
